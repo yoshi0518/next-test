eb7323353adb723da41b2e97227c5573d97b9538
yoshi0518 <yoshihumi.n.0518@gmail.com>
2025-05-10 19:43:27 +0900

    [test4]会社案内ページを追加

--- website/test4/src/common/lib/utils.ts
+++ website/test4/src/common/lib/utils.ts
@@ -1,7 +1,12 @@
 import type { ClassValue } from 'clsx';
+import { TZDate } from '@date-fns/tz';
 import { clsx } from 'clsx';
+import { format } from 'date-fns';
 import { twMerge } from 'tailwind-merge';
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const getJstDt = (date: Date, fmt = 'yyyy-MM-dd HH:mm:ss') =>
+  format(new TZDate(new Date(date), 'Asia/Tokyo'), fmt);
